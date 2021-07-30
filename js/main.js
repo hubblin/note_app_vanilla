1e6a37cf84a0f78b22dabdb4c70fccb0a9d8f030
hubblin <lhkworld16@gmail.com>
2021-07-30 18:02:38 +0900

    build sidebar

--- js/main.js
+++ js/main.js
@@ -1,19 +1,29 @@
-const btnEl = document.querySelector('#btn');
 const container = document.querySelector('.color_selector');
-const plusBtn = document.querySelector('.gooey.material-icons');
+const plusBtn = document.querySelector('.gooey.icons');
+const gooeyEls = document.querySelectorAll('.gooey.el');
 
 let hide = false;
 plusBtn.addEventListener('click', function(){
     hide = !hide;
     if(hide){
-        container.classList.add('a');
         gsap.to(plusBtn, .4, {
             rotate: 360
-        })
+        });
+        gooeyEls.forEach(function(gooeyEl, index){
+            gsap.to(gooeyEl, 1, {
+                delay: index * .3,
+                top: 60+ index * 30
+            })
+        })        
+
     }else{
-        container.classList.remove('a');
         gsap.to(plusBtn, .4, {
             rotate: 0
+        })
+        gooeyEls.forEach(function(gooeyEl, index){
+            gsap.to(gooeyEl, .8, {
+                top: 0
+            })
         })
     }
 })
